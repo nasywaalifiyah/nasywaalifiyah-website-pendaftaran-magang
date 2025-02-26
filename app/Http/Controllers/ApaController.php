6b450db8606cda18d88f517c0df20918ee6d8f6b
nasywaalifiyah <nasywaalifiya48@gmail.com>
2025-02-26 22:01:26 +0700

    first commit

---
+++ app/Http/Controllers/ApaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ApaController extends Controller
+{
+    
+    public function apa()
+    {
+        return view ('apa');
+    }
+}
---
+++ resources/views/apa.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Selamat Datang di Home</h1>
+    <p>Ini adalah halaman utama dengan navbar.</p>
+@endsection
